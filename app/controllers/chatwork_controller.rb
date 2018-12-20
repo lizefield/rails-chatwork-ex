d7395e4d1c8538a17efb8823727139c885823e94
Koji Iino <lizefield@gmail.com>
2018-12-20 11:40:51 +0900

    send途中

--- app/controllers/chatwork_controller.rb
+++ app/controllers/chatwork_controller.rb
@@ -3,12 +3,15 @@ require 'uri'
 
 class ChatworkController < ApplicationController
   BASE_ENDPOINT = 'https://api.chatwork.com/v2'
-  API_TOKEN = 'xxxxx'
+  API_TOKEN = '53e170e0e6e1e83989b0672904df3129'
 
   def show
-    # チャットルームに参加しているメンバー情報の取得
-    get_from_chatwork('rooms/xxxxx/members')
+    # 自分の情報
+    get_from_chatwork('me')
+    # コンタクト一覧
     # get_from_chatwork('contacts')
+    # チャットルームに参加しているメンバー情報の取得
+    # get_from_chatwork('rooms/xxxxx/members')
   end
 
   def send_message
@@ -32,7 +35,45 @@ class ChatworkController < ApplicationController
       JSON.parse(res.body)
     end
   end
+
+  def send_to_chatwork(room_id, message)
+    uri = URI.parse("#{BASE_ENDPOINT}/rooms/#{room_id}/messages")
+    http = Net::HTTP.new(uri.host, uri.port)
+
+    if uri.scheme == 'https'
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+
+    req = Net::HTTP::Post.new(uri.path)
+    req['X-ChatWorkToken'] = API_TOKEN
+    req.set_form_data({ body: message })
+
+    res = http.request(req)
+  end
 end
+
+# 自分の情報
+# {
+# 	"account_id"=>3570000, 
+# 	"room_id"=>135970000, 
+# 	"name"=>"山田 太郎", 
+# 	"chatwork_id"=>"", 
+# 	"organization_id"=>2350000, 
+# 	"organization_name"=>"", 
+# 	"department"=>"", 
+# 	"title"=>"", 
+# 	"url"=>"", "introduction"=>"", 
+# 	"mail"=>"", 
+# 	"tel_organization"=>"", 
+# 	"tel_extension"=>"", 
+# 	"tel_mobile"=>"", 
+# 	"skype"=>"", 
+# 	"facebook"=>"", 
+# 	"twitter"=>"", 
+# 	"avatar_image_url"=>"https://appdata.chatwork.com/avatar/ico_default_red.png", 
+# 	"login_mail"=>"0000@gmail.com"
+# }
 
 # チャットルーム参加者の一覧は以下の情報が取得出来る
 # [{
